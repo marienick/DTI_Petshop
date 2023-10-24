35bc18b2588e95f8dbf9271d502326eabe7c7076
Nicole Marie <127318149+marienick@users.noreply.github.com>
2023-10-24 09:47:22 -0300

    Add files via upload
    Organizei o projeto em três pastas distintas:
    Controller: Nesta pasta, adicionei três objetos Petshop à classe Petshop. Também implementei funções relacionadas ao cálculo do dia da semana e à determinação do melhor Petshop.
    Model: Esta pasta abriga a classe Petshop e seus atributos.
    View: Aqui, organizei as informações visíveis para o usuário.

---
+++ petshop/Controllers/Controller.cs
@@ -0,0 +1,77 @@
+﻿using Model;
+namespace Controllers
+{
+    public partial class Controller
+    {
+        private Petshop[] petshops;
+        public Controller()
+        {
+            this.petshops = new Petshop[3];
+            this.petshops[0] = new Petshop("Meu Canino Feliz", 2.0m, 48.0M, 24.0M, 20.0M, 40.0M);
+            this.petshops[1] = new Petshop("Vai Rex", 1.7m, 55.0M, 20.0M, 15.0M, 50.0M);
+            this.petshops[2] = new Petshop("ChowChawgas", 0.8m, 45.0M, 30.0M, 30.0M, 45.0M);
+        }
+        public static bool diaDaSemana(int ano, int mes, int dia)
+        {
+            DateTime dateValue = new DateTime(ano, mes, dia);
+            int data = ((int)dateValue.DayOfWeek);
+            if (data >= 1 && data <= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string melhorPetshop(bool data, int pequeno, int grande, out decimal preco)
+        {
+            Petshop melhorpet = petshops[0];
+            if (data)
+            {
+                for (int i = 1; i < petshops.Length; i++)
+                {
+                    if ((melhorpet.ValorGrande * grande) + (melhorpet.ValorPequeno * pequeno) > (petshops[i].ValorGrande * grande) + (petshops[i].ValorPequeno * pequeno))
+                    {
+                        melhorpet = petshops[i];
+
+                    }
+                    else if ((melhorpet.ValorGrande * grande) + (melhorpet.ValorPequeno * pequeno) == (petshops[i].ValorGrande * grande) + (petshops[i].ValorPequeno * pequeno))
+                    {
+                        if (melhorpet.Distancia > petshops[i].Distancia)
+                        {
+                            melhorpet = petshops[i];
+                        }
+                    }
+                }
+                preco = (melhorpet.ValorGrande * grande) + (melhorpet.ValorPequeno * pequeno);
+            }
+            else
+            {
+                for (int i = 1; i < petshops.Length; i++)
+                {
+                    if ((melhorpet.ValorGrandeFimSemana * grande) + (melhorpet.ValorPequenoFimSemana * pequeno) > (petshops[i].ValorGrandeFimSemana * grande) + (petshops[i].ValorPequenoFimSemana * pequeno))
+                    {
+                        melhorpet = petshops[i];
+
+                    }
+                    else if ((melhorpet.ValorGrandeFimSemana * grande) + (melhorpet.ValorPequenoFimSemana * pequeno) == (petshops[i].ValorGrandeFimSemana * grande) + (petshops[i].ValorPequenoFimSemana * pequeno))
+                    {
+                        if (melhorpet.Distancia > petshops[i].Distancia)
+                        {
+                            melhorpet = petshops[i];
+                        }
+                    }
+                }
+                preco = (melhorpet.ValorGrandeFimSemana * grande) + (melhorpet.ValorPequenoFimSemana * pequeno);
+            }
+            return melhorpet.Nome;
+
+
+
+        }
+    }
+
+}
+
+
---
+++ petshop/View/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Model;
+using Controllers;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Controller controller = new Controller();
+        int qtdPequeno, qtdGrande, dia, mes, ano;
+        decimal preco;
+        string dados;
+        Console.WriteLine("Digite o dia/mês/ano e a quantidade de cachorros pequenos e grandes que você deseja mandar para tomar banho:\nExemplo: 03/08/2018 3 5");
+        dados = Console.ReadLine();
+        string[] array = dados.Split('/');
+        dia = int.Parse(array[0]);
+        mes = int.Parse(array[1]);
+        string[] array2 = array[2].Split(" ");
+        ano = int.Parse(array2[0]);
+        qtdPequeno = int.Parse(array2[1]);
+        qtdGrande = int.Parse(array2[2]);
+        bool data = Controller.diaDaSemana(ano, mes, dia);
+        string melhorPetshop = controller.melhorPetshop(data, qtdPequeno, qtdGrande, out preco);
+        Console.WriteLine(melhorPetshop + " " + preco);
+    }
+}
