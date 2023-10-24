9c8d4b631138b4fd673868c7db10a4c57bb74454
Nicole Marie <127318149+marienick@users.noreply.github.com>
2023-10-24 09:49:51 -0300

    Update Petshop.cs
    Criei a classe petshop com os atributos

--- petshop/Model/Petshop.cs
+++ petshop/Model/Petshop.cs
@@ -1,12 +1,23 @@
-namespace Petshop.Models
+namespace Model
 {
+
     public partial class Petshop
     {
-        public decimal distancia {get; set;}
-        public decimal valorPequeno { get; set;}
-        public decimal valorGrande { get; set;}
-        public decimal valorPequenoFimSemana { get; set;}
-        public decimal valorGrandeFimSemana { get;set;}
-        public string nome { get; set;}
+        public decimal Distancia { get; set; }
+        public decimal ValorPequeno { get; set; }
+        public decimal ValorGrande { get; set; }
+        public decimal ValorPequenoFimSemana { get; set; }
+        public decimal ValorGrandeFimSemana { get; set; }
+        public string Nome { get; set; }
+        public Petshop(string petshop, decimal km, decimal valorGrandeFinalSemana, decimal valorPequenoFinalsemana, decimal valorPequeno, decimal valorGrande)
+        {
+            Distancia = km;
+            Nome = petshop;
+            ValorGrande = valorGrande;
+            ValorPequeno = valorPequeno;
+            ValorPequenoFimSemana = valorPequenoFinalsemana;
+            ValorGrandeFimSemana = valorGrandeFinalSemana;
+        }
+
     }
 }
