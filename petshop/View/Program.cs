4bef8b0257d4a4a69811a5b2b0e384c40539b019
Nicole Marie <127318149+marienick@users.noreply.github.com>
2023-10-25 11:05:59 -0300

    Corrigi uma falha que estava tendo no código

--- petshop/View/Program.cs
+++ petshop/View/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Model;
 using Controllers;
+
 class Program
 {
-
     static void Main(string[] args)
     {
+        //criei um objeto controller
         Controller controller = new Controller();
         int qtdPequeno, qtdGrande, dia, mes, ano;
         decimal preco;
@@ -19,8 +20,11 @@ class Program
         ano = int.Parse(array2[0]);
         qtdPequeno = int.Parse(array2[1]);
         qtdGrande = int.Parse(array2[2]);
+        //esse Controller antes das funções significa que essas funções pertencem a classe Controller
         bool data = Controller.diaDaSemana(ano, mes, dia);
-        string melhorPetshop = controller.melhorPetshop(data, qtdPequeno, qtdGrande, out preco);
-        Console.WriteLine(melhorPetshop + " " + preco);
+        //Já esse controller na frente da função melhorPetshop e na frente do vetor petshops é para que as informacões de petshops sejam "iniciadas" no contexto atual
+        string melhorLugar = controller.melhorPetshop(controller.petshops, data, qtdPequeno, qtdGrande, out preco);
+        Console.WriteLine(melhorLugar + " " + preco);
+
     }
 }
