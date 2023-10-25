a00909a725b87c79fc8d8c6bef0fa6e0ec3923db
Nicole Marie <127318149+marienick@users.noreply.github.com>
2023-10-24 21:27:21 -0300

    substitui a função antiga de encontrar o melhor petshop que estava redundante por um sucinto

--- petshop/Controllers/Controller.cs
+++ petshop/Controllers/Controller.cs
@@ -1,9 +1,11 @@
-﻿using Model;
+using Model;
 namespace Controllers
 {
     public partial class Controller
     {
-        private Petshop[] petshops;
+        public Petshop[] petshops;
+
+        //Construtor que é usado para criar novos petshops (objetos)
         public Controller()
         {
             this.petshops = new Petshop[3];
@@ -11,8 +13,35 @@ namespace Controllers
             this.petshops[1] = new Petshop("Vai Rex", 1.7m, 55.0M, 20.0M, 15.0M, 50.0M);
             this.petshops[2] = new Petshop("ChowChawgas", 0.8m, 45.0M, 30.0M, 30.0M, 45.0M);
         }
+        
+        //função para achar o petshop mais acessível
+        public static string melhorPetshop(Petshop[] petshops, bool data, int qtdPequeno, int qtdGrande, out decimal preco)
+        {
+            Petshop melhorOpcao = petshops[0];
+            for (int i = 1; i < petshops.Length; i++)
+            {
+                if (melhorOpcao.calcularValor(data, qtdPequeno, qtdGrande) > petshops[i].calcularValor( data, qtdPequeno, qtdGrande))
+                {
+                    melhorOpcao = petshops[i];
+                    
+                }
+                else if (melhorOpcao.calcularValor(data, qtdPequeno, qtdGrande) == petshops[i].calcularValor(data, qtdPequeno, qtdGrande))
+                {
+                    if (melhorOpcao.distancia > petshops[i].distancia)
+                    {
+                        melhorOpcao = petshops[i];
+                    }
+                }
+            }
+            preco = melhorOpcao.calcularValor(data, qtdPequeno, qtdGrande);
+            return melhorOpcao.nome;
+        }
+        
+
+        //função para saber se é ou não final de semana
         public static bool diaDaSemana(int ano, int mes, int dia)
         {
+            
             DateTime dateValue = new DateTime(ano, mes, dia);
             int data = ((int)dateValue.DayOfWeek);
             if (data >= 1 && data <= 5)
@@ -24,54 +53,7 @@ namespace Controllers
                 return false;
             }
         }
-        public string melhorPetshop(bool data, int pequeno, int grande, out decimal preco)
-        {
-            Petshop melhorpet = petshops[0];
-            if (data)
-            {
-                for (int i = 1; i < petshops.Length; i++)
-                {
-                    if ((melhorpet.ValorGrande * grande) + (melhorpet.ValorPequeno * pequeno) > (petshops[i].ValorGrande * grande) + (petshops[i].ValorPequeno * pequeno))
-                    {
-                        melhorpet = petshops[i];
-
-                    }
-                    else if ((melhorpet.ValorGrande * grande) + (melhorpet.ValorPequeno * pequeno) == (petshops[i].ValorGrande * grande) + (petshops[i].ValorPequeno * pequeno))
-                    {
-                        if (melhorpet.Distancia > petshops[i].Distancia)
-                        {
-                            melhorpet = petshops[i];
-                        }
-                    }
-                }
-                preco = (melhorpet.ValorGrande * grande) + (melhorpet.ValorPequeno * pequeno);
-            }
-            else
-            {
-                for (int i = 1; i < petshops.Length; i++)
-                {
-                    if ((melhorpet.ValorGrandeFimSemana * grande) + (melhorpet.ValorPequenoFimSemana * pequeno) > (petshops[i].ValorGrandeFimSemana * grande) + (petshops[i].ValorPequenoFimSemana * pequeno))
-                    {
-                        melhorpet = petshops[i];
-
-                    }
-                    else if ((melhorpet.ValorGrandeFimSemana * grande) + (melhorpet.ValorPequenoFimSemana * pequeno) == (petshops[i].ValorGrandeFimSemana * grande) + (petshops[i].ValorPequenoFimSemana * pequeno))
-                    {
-                        if (melhorpet.Distancia > petshops[i].Distancia)
-                        {
-                            melhorpet = petshops[i];
-                        }
-                    }
-                }
-                preco = (melhorpet.ValorGrandeFimSemana * grande) + (melhorpet.ValorPequenoFimSemana * pequeno);
-            }
-            return melhorpet.Nome;
-
-
-
-        }
+        
     }
 
 }
-
-
